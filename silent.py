dd8a4e46a7d5352cba614ddcb3899b3411c6b8e3
Nick Loadholtes <nick@ironboundsoftware.com>
2019-01-25 09:44:39 -0500

    Improving import error checking

--- silent.py
+++ silent.py
@@ -1,5 +1,9 @@
-import weechat
+try:
+    import weechat
+except ImportError:
+    print("This script must be run from inside weechat")
+    exit -1
 
 weechat.register("silent_python", "Silent", "1.0", "MIT", "Make a user silent", "", "")
-weechat.prnt("", "Silent Python started")
+weechat.prnt("", "Silent Python started?")
 
