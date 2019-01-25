4db8b45e8ed5bc38e1cef95fe7856a5b6815e2c1
Nick Loadholtes <nick@ironboundsoftware.com>
2019-01-25 10:41:47 -0500

    adding end handler, organizing things a bit

--- silent.py
+++ silent.py
@@ -4,6 +4,13 @@ except ImportError:
     print("This script must be run from inside weechat")
     exit -1
 
-weechat.register("silent_python", "Silent", "1.0", "MIT", "Make a user silent", "", "")
-weechat.prnt("", "Silent Python started?")
 
+def silent_end():
+    """Looks like we were told to stop"""
+    return weechat.WEECHAT_RC_OK
+
+
+if __name__ == "__main__":
+    weechat.register("silent_python", "Silent", "1.0", "MIT",
+                     "Make a user silent", "silent_end", "")
+    weechat.prnt("", "Silent Python started?")
